aeea547a9b0f142748fbd4558f873b557091c442
Tri Denda <34482931+tridenda@users.noreply.github.com>
2022-02-18 07:12:16 +0700

    big bang

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World! </Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- README.md
+++
@@ -1,2 +0,0 @@
-# AddictionDetectionSystem
-A system that can detect online game addiction using certainty factor algorithm.
