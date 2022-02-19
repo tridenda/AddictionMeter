52d49a2b29e7235b5e70252d8aede207c75f21fb
Tri Denda <34482931+tridenda@users.noreply.github.com>
2022-02-19 21:22:35 +0700

    fix things

--- src/features/menu/screens/menu.screen.js
+++ src/features/menu/screens/menu.screen.js
@@ -1,60 +1,61 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
-import { ScrollView } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import styled from "styled-components";
 
-import { SafeArea } from "../../../components/utility/safe-area.component";
-
 const ContainerMenu = styled.View`
+  flex: 1;
   flex-wrap: wrap;
   flex-direction: row;
   align-items: flex-start;
   justify-content: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 
 const ButtonMenu = styled.View`
+  width: 150px;
+  height: 150px;
   background-color: red;
   margin: 5px;
-  width: 45%;
-  height: 100%;
+  align-items: center;
+  justify-content: center;
 `;
 
 const TitleMenu = styled.Text``;
 
 export const MenuScreen = () => {
   return (
-    <SafeArea>
-      <ScrollView>
-        <ContainerMenu>
-          <ButtonMenu>
-            <TitleMenu>1</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>2</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>3</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>4</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>5</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>5</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>5</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>5</TitleMenu>
-          </ButtonMenu>
-          <ButtonMenu>
-            <TitleMenu>5</TitleMenu>
-          </ButtonMenu>
-        </ContainerMenu>
-      </ScrollView>
-    </SafeArea>
+    <ScrollView>
+      <ContainerMenu>
+        <ButtonMenu>
+          <TitleMenu>Deteksi Kecanduan</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Riwayat Kecanduan</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Data Gejala</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Data Kecanduan</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Tingkat Kecanduan</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Data Aturan</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Tentang Aplikasi</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Profil Pengguna</TitleMenu>
+        </ButtonMenu>
+        <ButtonMenu>
+          <TitleMenu>Keluar</TitleMenu>
+        </ButtonMenu>
+      </ContainerMenu>
+    </ScrollView>
   );
 };
